f6378a7973649a02f9c43f881e7fa7b2e365325c
Karso2023 <karsoo2023@gmail.com>
2026-01-28 19:13:46 +0000

    project structure

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Moon, Sun } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://vercel.com/templates?framework=next.jcd ..s&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
